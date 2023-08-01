1a31273b1109e378c07126a586270dbb06b570e9
JI-Maina <irungujmaina@gmail.com>
2023-08-01 22:19:18 +0300

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """Defines homepage"""
-    return 0-index.html
+    return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
