e0ae89cd43e653f44b00875bc591e70f87a97c44
JI-Maina <irungujmaina@gmail.com>
2023-08-02 10:19:11 +0300

    Added babel.cfg

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Parametrizes templates
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """configures available languages in our app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index() -> str:
+    """Defines homepage"""
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determines the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
